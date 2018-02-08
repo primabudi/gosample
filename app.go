8683937ade0d2185136d27d7d4fd40802ad76532
Andi Muhamad Primabudi <andi.primabudi@tokopedia.com>
2018-02-08 12:44:34 +0700

    primabudi

--- app.go
+++ app.go
@@ -1,40 +1,98 @@
 package main
 
 import (
-	"flag"
-	"github.com/google/gops/agent"
+	"database/sql"
 	"log"
+	"strconv"
+	"time"
+
 	"net/http"
 
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-
-	"github.com/tokopedia/gosample/hello"
-	"github.com/tokopedia/logging/tracer"
-	"gopkg.in/tokopedia/grace.v1"
-	"gopkg.in/tokopedia/logging.v1"
+	_ "github.com/lib/pq"
 )
 
+var dbHome *sql.DB
+var err error
+var counter = 0
+
+type (
+	User struct {
+		ID          int
+		Name        string
+		Msisdn      string
+		Email       string
+		BirthDate   time.Time
+		CreatedTime time.Time
+		UpdateTime  time.Time
+		Age         int
+	}
+)
+
+var listUser = []User{}
+
 func main() {
-
-	flag.Parse()
-	logging.LogInit()
-
-	debug := logging.Debug.Println
-
-	debug("app started") // message will not appear unless run with -debug switch
-
-	if err := agent.Listen(&agent.Options{}); err != nil {
-		log.Fatal(err)
+	dbHome, err = sql.Open("postgres", "postgres://st140804:apaajadeh@devel-postgre.tkpd/tokopedia-user?sslmode=disable")
+	if err != nil {
+		log.Print(err)
 	}
 
-	hwm := hello.NewHelloWorldModule()
+	doQuery()
 
-	http.Handle("/metrics", promhttp.Handler())
+	http.HandleFunc("/html", handleHTML)
+	http.ListenAndServe(":8080", nil)
+}
 
-	http.HandleFunc("/hello", hwm.SayHelloWorld)
-	go logging.StatsLog()
+func doQuery() {
+	usr := &User{}
+	stmt := "select user_id, full_name, msisdn, user_email, birth_date, create_time, update_time, DATE_PART('year', NOW()::date) - DATE_PART('year', birth_date::date) from ws_user ORDER BY user_id ASC limit 10"
+	rows, err := dbHome.Query(stmt)
 
-	tracer.Init(&tracer.Config{Port: 8700, Enabled: true})
+	for rows.Next() {
+		_ = rows.Scan(&usr.ID, &usr.Name, &usr.Msisdn, &usr.Email, &usr.BirthDate, &usr.CreatedTime, &usr.UpdateTime, &usr.Age)
+		listUser = append(listUser, *usr)
+		//log.Print(usr.ID, " - ", usr.Name, " - ", usr.Msisdn, " - ", usr.BirthDate, " - ", usr.Email, " - ", usr.CreatedTime, " - ", usr.UpdateTime)
+	}
 
-	log.Fatal(grace.Serve(":9000", nil))
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func handleHTML(w http.ResponseWriter, r *http.Request) {
+	counter = counter + 1
+	strr :=
+		"<html>" +
+			"<body>" +
+
+			"<table style>" +
+			"<tr>" +
+			"	<th>ID</th>" +
+			"	<th>Name</th>" +
+			"	<th>MSISDN</th>" +
+			"	<th>Email</th>" +
+			"	<th>Birthdate</th>" +
+			"	<th>CreateTime</th>" +
+			"	<th>UpdateTime</th>" +
+			"	<th>Age</th>" +
+			"</tr>"
+
+	for i := 0; i < len(listUser); i++ {
+
+		strr += "<tr>" +
+			"<td>" + strconv.Itoa(listUser[i].ID) + "</td>" +
+			"<td>" + listUser[i].Name + "</td>" +
+			"<td>" + listUser[i].Msisdn + "</td>" +
+			"<td>" + listUser[i].Email + "</td>" +
+			"<td>" + listUser[i].BirthDate.String() + "</td>" +
+			"<td>" + listUser[i].CreatedTime.String() + "</td>" +
+			"<td>" + listUser[i].UpdateTime.String() + "</td>" +
+			"<td>" + strconv.Itoa(listUser[i].Age) + "</td>" +
+			"</tr>"
+	}
+	strr += "</table>" +
+		"visit counter = " + strconv.Itoa(counter) +
+		"</body>" +
+		"</html>"
+	byt := []byte(strr)
+	w.Write(byt)
 }
